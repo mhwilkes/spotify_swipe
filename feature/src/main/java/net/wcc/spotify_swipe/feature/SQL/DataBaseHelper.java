c5d6f3a0c4530c6c6e9647823b5ea6b357486e62
Chris <christopherjohn006@gmail.com>
2018-12-17 15:04:53 -0500

    added SQL classes, and db prototype

---
+++ feature/src/main/java/net/wcc/spotify_swipe/feature/SQL/DataBaseHelper.java
@@ -0,0 +1,75 @@
+package net.wcc.spotify_swipe.feature;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private SQLiteDatabase db;
+    private final Context context;
+    private static String DB_PATH = "";
+    private static String DB_NAME = "androidDB.db";
+
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        this.context = context;
+    }
+
+    public void createDataBase() {
+
+        File dbFile = new File(DB_PATH + DB_NAME);
+        boolean dataBaseExist = dbFile.exists();
+        if(!dataBaseExist) {
+            this.getReadableDatabase();
+            this.close();
+            try {
+                copyDataBase();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void copyDataBase() throws IOException {
+        InputStream input = context.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream output = new FileOutputStream(outFileName);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = input.read(buffer))>0)
+        {
+            output.write(buffer, 0, len);
+        }
+        output.flush();
+        output.close();
+        input.close();
+    }
+
+    public boolean openDataBase() throws SQLException {
+        String path = DB_PATH + DB_NAME;
+        db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        return db != null;
+    }
+
+    @Override
+    public synchronized void close() {
+        if(db != null)
+            db.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
+}
---
+++ feature/src/main/java/net/wcc/spotify_swipe/feature/SQL/SQLHandler.java
@@ -0,0 +1,56 @@
+package net.wcc.spotify_swipe.feature;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class SQLHandler {
+
+    private SQLiteDatabase db;
+    private DataBaseHelper dbHelper;
+
+    public SQLHandler(Context context) {
+        dbHelper = new DataBaseHelper(context);
+    }
+
+    public SQLHandler createDatabase() throws SQLException {
+        try {
+            dbHelper.createDataBase();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return this;
+    }
+
+    public SQLHandler open() throws SQLException {
+        try {
+            dbHelper.openDataBase();
+            dbHelper.close();
+            db = dbHelper.getWritableDatabase();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public Cursor query(String sql) {
+        try {
+            Cursor cursor = db.rawQuery(sql, null);
+            return cursor;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int update(String table, ContentValues cv, String where, String[] whereArgs) {
+        return db.update(table, cv, where, whereArgs);
+    }
+}
